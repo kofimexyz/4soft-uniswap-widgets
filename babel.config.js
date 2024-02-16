b40f22f5e371bc477f4d341699a6a39e04452e5a
Bartosz Jabłoński <bjablonski@codeinthecup.pl>
2024-02-16 10:20:47 +0100

    Fix: make react bundling automatic to work with nextjs

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,7 @@ module.exports = {
         // Ship with 'classic' runtime for compatibility with React >=17, otherwise ESM module resolution differs
         // between versions (see https://github.com/facebook/react/issues/20235); develop with 'automatic' runtime for
         // ease of development, so that React does not need to be top-level imported everywhere.
-        runtime: isDevelopment ? 'automatic' : 'classic',
+        runtime: isDevelopment ? 'automatic' : 'automatic`',
       },
     ],
     ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
